ebbe57b402c6926a2d1a3e947053c5706b2606af
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-19 02:32:21 -0400

    client basic app, set env

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CIndex from './components/components.index'
+
+function App() {
+	const { ContactForm } = CIndex
+
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<ContactForm />
+		</div>
+	)
+}
+
+export default App
